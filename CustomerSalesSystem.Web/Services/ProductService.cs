4ca3b4864bd012c1a8ff7feac38f877a06420028
Md Golam Mahmud <golam.mahmud99@gmail.com>
2025-05-16 18:07:31 +0600

    added the web project

---
+++ CustomerSalesSystem.Web/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using CustomerSalesSystem.Application.DTOs;
+using CustomerSalesSystem.Application.Features.Products.Commands;
+
+namespace CustomerSalesSystem.Web.Services
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<ProductDto>> GetAllAsync() =>
+            await _httpClient.GetFromJsonAsync<List<ProductDto>>("api/products");
+
+        public async Task<ProductDto?> GetByIdAsync(int id) =>
+            await _httpClient.GetFromJsonAsync<ProductDto>($"api/products/{id}");
+
+        public async Task CreateAsync(CreateProductCommand product) =>
+            await _httpClient.PostAsJsonAsync("api/products", product);
+
+        public async Task UpdateAsync(UpdateProductCommand product) =>
+            await _httpClient.PutAsJsonAsync($"api/products/{product.Id}", product);
+
+        public async Task DeleteAsync(int id) =>
+            await _httpClient.DeleteAsync($"api/products/{id}");
+    }
+
+}
